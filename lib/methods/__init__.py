4b421aab3d982b571be7fba20669b60208e0c7fe
jul-zh <zhidkova.ym@gmail.com>
2026-04-08 16:02:48 +0300

    Update __init__.py

--- lib/methods/__init__.py
+++ lib/methods/__init__.py
@@ -3,12 +3,15 @@ from .my_baseline import MY_BASELINE
 from .my_baseline_sdefeat import MY_BASELINE_SDEFEAT
 from .my_baseline_sdelight import MY_BASELINE_SDELIGHT
 from .my_baseline_sdelight_prob import MY_BASELINE_SDELIGHT_PROB
+from .my_baseline_rbfkan_prob import MY_BASELINE_RBFKAN_PROB
+
 method_maps = {
     'cotere': COTERE,
     'my_baseline': MY_BASELINE,
     'my_baseline_sdefeat': MY_BASELINE_SDEFEAT,
     'my_baseline_sdelight': MY_BASELINE_SDELIGHT,
-    'my_baseline_sdelight_prob': MY_BASELINE_SDELIGHT_PROB
+    'my_baseline_sdelight_prob': MY_BASELINE_SDELIGHT_PROB,
+    'my_baseline_rbfkan_prob': MY_BASELINE_RBFKAN_PROB
 }
 
 __all__ = [
@@ -16,6 +19,7 @@ __all__ = [
     'MY_BASELINE',
     'MY_BASELINE_SDEFEAT',
     'MY_BASELINE_SDELIGHT',
-    'MY_BASELINE_SDELIGHT_PROB'
+    'MY_BASELINE_SDELIGHT_PROB',
+    'MY_BASELINE_RBFKAN_PROB'
 ]
 
