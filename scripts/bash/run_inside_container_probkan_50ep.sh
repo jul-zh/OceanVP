8378fccf1a8aa4a94b045dd63a4683b9744fec31
jul-zh <zhidkova.ym@gmail.com>
2026-04-09 18:01:14 +0300

    Create run_inside_container_probkan_50ep.sh

---
+++ scripts/bash/run_inside_container_probkan_50ep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/yzhidkova/projects/OceanVP
+export PYTHONPATH=/home/yzhidkova/projects/OceanVP:$PYTHONPATH
+
+echo "=== TRAIN PROBKAN START ==="
+hostname
+pwd
+python --version
+
+python tools/train.py \
+    -d ocean_t0_32_64 \
+    -c configs/ocean/t0_32_64/PROBKAN.py \
+    --ex_name probkan_oceanvp_10ep \
+    --temp_stride 2 \
+    --epoch 50
